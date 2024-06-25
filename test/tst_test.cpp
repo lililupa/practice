cf68d3acd38168f922f333322242142d6702c60a
Ковшик Арина С. <fpm.kovshikAS@bsu.by>
2024-06-25 10:54:14 +0300

    seecond commit

--- test/tst_test.cpp
+++ test/tst_test.cpp
@@ -23,6 +23,8 @@ test::~test() {}
 void test::test_case1() {
     Nod nod;
     QCOMPARE(nod.findNod(10, 5), 5);
+    QCOMPARE(nod.findNod(10, 11), 1);
+    QCOMPARE(nod.findNod(52, 56), 4);
 }
 
 QTEST_APPLESS_MAIN(test)
